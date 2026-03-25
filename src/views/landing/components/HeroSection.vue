3df77c6cd64222c6773026c99fb19ca0a96e38af
Jetross <jetrossneri07@gmail.com>
2026-03-25 11:58:02 +0800

    feat: add typography

--- src/views/landing/components/HeroSection.vue
+++ src/views/landing/components/HeroSection.vue
@@ -5,22 +5,26 @@ defineOptions({
 
 import Badge from '@/components/ui/badge/Badge.vue'
 import Button from '@/components/ui/button/Button.vue'
+import TypographyH1 from '@/components/typography/TypographyH1.vue'
+import TypographyMuted from '@/components/typography/TypographyMuted.vue'
 </script>
 
 <template>
   <section class="relative overflow-hidden text-white py-20">
-    <div class="container mx-auto px-6 md:px-12 lg:px-20 flex flex-col items-center text-center">
+    <div
+      class="container mx-auto px-6 md:px-12 lg:px-20 mt-18 flex flex-col items-center text-center"
+    >
       <Badge variant="outline" class="mb-4">Start Here</Badge>
-      <h1 class="text-4xl md:text-5xl font-semibold text-foreground mb-6">
+      <TypographyH1 class="mb-2">
         CHOOSE IDEAL LOCATION <span class="text-accent">INSTANTLY</span>
-      </h1>
-      <p class="text-lg text-muted-foreground md:text-xl mb-8 max-w-2xl">
+      </TypographyH1>
+      <TypographyMuted class="text-lg md:text-xl mb-4 max-w-2xl">
         An AI-powered system that helps choose business locations and connects local businesses in
         your city
-      </p>
+      </TypographyMuted>
       <div class="flex gap-4">
         <Button as-child size="sm">
-          <RouterLink to="/">Get Started</RouterLink>
+          <RouterLink to="/">Join Us</RouterLink>
         </Button>
         <Button as-child variant="ghost" size="sm">
           <RouterLink to="/learn-more">Learn More</RouterLink>
