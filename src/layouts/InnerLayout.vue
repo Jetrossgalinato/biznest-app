f9cf2cc6c322dcd200af7fa4f1213f74cafb1c0b
Jetross <jetrossneri07@gmail.com>
2026-03-27 11:20:38 +0800

    feat: add InnerNavbar component

--- src/layouts/InnerLayout.vue
+++ src/layouts/InnerLayout.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import InnerNavbar from '@/components/InnerNavbar.vue'
 </script>
 
 <template>
+  <InnerNavbar />
   <main class="mx-auto w-full max-w-6xl px-4 py-6">
     <RouterView />
   </main>
